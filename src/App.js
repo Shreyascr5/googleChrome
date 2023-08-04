0a4ab1d622a23fc78dd773f724e54ee40aa0e1eb
Shreyascr5 <shreyas050801@gmail.com>
2023-08-04 23:20:09 +0530

    added the main body and footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from './Layouts/Header';
 import Main from './Layouts/Main';
-
+import Footer from './Layouts/Footer';
 
 function App() {
   return (
     <div className="App">
      <Header/>
      <Main/>
+     <Footer/>
     </div>
   );
 }
---
+++ src/Layouts/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="country">India</div>
+      <div className="info">
+        <a href="/" id="first">
+          About  Advertising Business How Search works </a>
+          <a id="second" href="/">Privacy Terms Settings
+        </a>
+      </div>
+    </div>
+  );
+}
